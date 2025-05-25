f06b3b035691eee60307a94fd4548764ddfc4973
NicoFuksman <bf0005@est.ort.edu.ar>
2025-05-25 16:05:35 -0300

    Parkour mejorado con killers

---
+++ Assets/MuertePorColision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MuertePorColision : MonoBehaviour
+{
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.name == "Killer")
+        {
+            Debug.Log("Me morí");
+            SceneManager.LoadScene("Parkour");
+        }
+    }
+}
