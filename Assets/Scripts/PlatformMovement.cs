dddaec9a5ac7a80ec62c0c0d63772ed591a8c39b
NicoFuksman <bf0005@est.ort.edu.ar>
2025-06-01 14:49:56 -0300

    Destructor de barriles y plataforma movil agregada

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+
+    public Transform[] puntosmoviles;
+    public float platformSpeed = 2f;
+
+    private int siguientePunto = 0;
+
+    void Update()
+    {
+        if (puntosmoviles.Length == 0) return;
+
+        Transform target = puntosmoviles[siguientePunto];
+        transform.position = Vector3.MoveTowards(transform.position, target.position, platformSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target.position) < 0.1f)
+        {
+            siguientePunto = (siguientePunto + 1) % puntosmoviles.Length;
+        }
+    }
+    }
+
