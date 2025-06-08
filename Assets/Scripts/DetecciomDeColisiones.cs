a7c5f94147c2c1a6882c9bff7938a760e11ac909
NicoFuksman <bf0005@est.ort.edu.ar>
2025-06-08 14:57:51 -0300

    Puntaje casi terminado + pinche

--- Assets/Scripts/DetecciomDeColisiones.cs
+++ Assets/Scripts/DetecciomDeColisiones.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class DetecciomDeColisiones : MonoBehaviour
 {
+    public ScoreManager scoreManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreManager = FindObjectOfType<ScoreManager>();
     }
 
     // Update is called once per frame
@@ -16,11 +17,13 @@ public class DetecciomDeColisiones : MonoBehaviour
         
     }
 
+
     private void OnCollisionEnter(Collision collision)
     {
        if (collision.gameObject.name == "Player")
         {
             Destroy(gameObject);
+            scoreManager.AddScore();
         }
     }
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+    public UIManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        manager = FindObjectOfType<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore()
+    {
+        score ++;
+        manager.UpdateScore(score);
+    }
+}
